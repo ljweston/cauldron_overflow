7a1b41c81c9224dc13f17490f72c08c78e13c936
ljweston <lweston@ncees.org>
2022-06-16 14:45:24 -0400

    Added new routes and template data

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,10 +2,11 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class QuestionController
+class QuestionController extends AbstractController
 {
     /**
      * @Route("/")
@@ -20,9 +21,22 @@ class QuestionController
      */
     public function show($slug)
     {
-        return new Response(sprintf(
-            'Future page to show a question "%s"!',
-            ucwords(str_replace('-', '', $slug)) 
-        ));
+        $answers = [
+            'Make sure the cat is sitting still',
+            'Fuzzy slippers',
+            'This isn\'t a question so much as it is a statement I think'
+        ];
+        // recall that the controllers always require a RESPONSE OBJ be returned.
+        // THUS: render returns a response object
+        return $this->render('questions/show.html.twig', [
+            // array of data or vars passed in
+            'question' => ucwords(str_replace('-', ' ', $slug)),
+            'answers' => $answers
+        ]);
+
+        // return new Response(sprintf(
+        //     'Future page to show a question "%s"!',
+        //     ucwords(str_replace('-', '', $slug)) 
+        // ));
     }
 }
