30f7c9773906b0e4222a8dc75077fdb9fe7d9f78
ljweston <lweston@ncees.org>
2022-06-17 13:48:50 -0400

    Environment example for html rendering

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class QuestionController extends AbstractController
 {
@@ -12,8 +13,11 @@ class QuestionController extends AbstractController
     /**
      * @Route("/", name="app_homepage")
      */
-    public function homepage()
+    public function homepage(Environment $twigEnvironment)
     {
+        // $html = $twigEnvironment->render('questions/homepage.html.twig'); // returns string with html
+
+        // return new Response($html);
         return $this->render('questions/homepage.html.twig');
     }
 
