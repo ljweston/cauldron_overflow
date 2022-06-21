820ce7805bf1ba759afbfeff536ab9acafd82444
ljweston <lweston@ncees.org>
2022-06-21 15:33:30 -0400

    Created a custom console command

--- src/Command/RandomSpellCommand.php
+++ src/Command/RandomSpellCommand.php
@@ -2,6 +2,7 @@
 
 namespace App\Command;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -16,28 +17,53 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 )]
 class RandomSpellCommand extends Command
 {
+    protected static $defaultName = 'app:random-spell';
+
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+        parent::__construct();
+    }
+
     protected function configure(): void
     {
         $this
-            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
-            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+            ->setDescription('Cast a random spell')
+            ->addArgument('your-name', InputArgument::OPTIONAL, 'Your name')
+            ->addOption('yell', null, InputOption::VALUE_NONE, 'Yell?')
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $io = new SymfonyStyle($input, $output);
-        $arg1 = $input->getArgument('arg1');
+        $io = new SymfonyStyle($input, $output); // rendering output
+        $yourName = $input->getArgument('your-name');
 
-        if ($arg1) {
-            $io->note(sprintf('You passed an argument: %s', $arg1));
+        if ($yourName) {
+            $io->note(sprintf('Hi there %s!', $yourName));
         }
 
-        if ($input->getOption('option1')) {
-            // ...
+        $spells = [
+            'alohomora',
+            'confundo',
+            'engorgio',
+            'expecto patronum',
+            'expelliarmus',
+            'impedimenta',
+            'reparo',
+        ];
+        
+        $spell = $spells[array_rand($spells)];
+        
+        if ($input->getOption('yell')) {
+            $spell = strtoupper($spell);
         }
 
-        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+        $this->logger->info('Casting spell '.$spell);
+
+        $io->success($spell);
 
         return Command::SUCCESS;
     }
