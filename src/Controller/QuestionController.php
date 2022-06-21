08551c0e8f68424b562d4f5e5d8bdb88609b0c60
ljweston <lweston@ncees.org>
2022-06-21 13:01:58 -0400

    Controller with updated env vars

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Service\MarkdownHelper;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,6 +11,13 @@ use Twig\Environment;
 
 class QuestionController extends AbstractController
 {
+    private $logger;
+    private $isDebug;
+    public function __construct(LoggerInterface $logger, bool $isDebug)
+    {
+        $this->logger = $logger;
+        $this->isDebug = $isDebug;
+    }
     // #[Route('/', name:"app_homepage")] // php 8 feature
     /**
      * @Route("/", name="app_homepage")
@@ -27,7 +35,10 @@ class QuestionController extends AbstractController
      */
     public function show($slug, MarkdownHelper $markdownHelper)
     {
-        dump($this->getParameter('cache_adapter'));
+        if ($this->isDebug) {
+            $this->logger->info('We are in debug mode');
+        }
+
         $answers = [
             'Make sure the cat is sitting `purrrfectly` still',
             'Fuzzy slippers',
