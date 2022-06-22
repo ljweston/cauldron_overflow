c4e4a3ae2fb93db906b9e6ceb4362a1a4b04cc8b
ljweston <lweston@ncees.org>
2022-06-22 15:55:51 -0400

    Updated template to accept question obj

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -89,19 +89,12 @@ class QuestionController extends AbstractController
             'Fuzzy slippers',
             'This isn\'t a question so much as it is a statement I think'
         ];
-        
-        $questionText = 'I\'ve been turned into a cat, any thoughts on how to turn back? While I\'m **adorable**, I don\'t really care for cat food.';
-        
-        $parsedQuestionText = $markdownHelper->parse($questionText);
-
-        // dump($this);
 
         // recall that the controllers always require a RESPONSE OBJ be returned.
         // THUS: render returns a response object
         return $this->render('questions/show.html.twig', [
             // array of data or vars passed in
-            'question' => ucwords(str_replace('-', ' ', $slug)),
-            'questionText' => $parsedQuestionText,
+            'question' => $question,
             'answers' => $answers
         ]);
 
