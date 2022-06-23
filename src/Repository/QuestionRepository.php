cd59053d0c48904e1f0ee079479036985c6eb7d6
ljweston <lweston@ncees.org>
2022-06-23 13:03:42 -0400

    Added helper methods for query building

--- src/Repository/QuestionRepository.php
+++ src/Repository/QuestionRepository.php
@@ -5,6 +5,7 @@ namespace App\Repository;
 use App\Entity\Question;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\DBAL\Query\QueryBuilder;
+use Doctrine\ORM\QueryBuilder as ORMQueryBuilder;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -45,17 +46,22 @@ class QuestionRepository extends ServiceEntityRepository
     */
    public function findAllAskedOrderByNewest(): array
    {
-       return $this->createQueryBuilder('q')
-           ->andWhere('q.askedAt IS NOT NULL')
+       return $this->addIsAskedQueryBuilder()
            ->orderBy('q.askedAt', 'DESC')
            ->getQuery() 
            ->getResult()
        ;
    }
-
-   private function addIsAskedQueryBuilder(QueryBuilder $qb): QueryBuilder
+   // Function to modify passed queryBuilder to accept what we pass in
+   private function addIsAskedQueryBuilder(ORMQueryBuilder $qb = null): ORMQueryBuilder
    {
-        return $qb->andWhere('q.askedAt IS NOT NULL');
+        return $this->getOrCreateQueryBuilder($qb)
+            ->andWhere('q.askedAt IS NOT NULL');
+   }
+
+   private function getOrCreateQueryBuilder(ORMQueryBuilder $qb = null): ORMQueryBuilder
+   {
+        return $qb ?: $this->createQueryBuilder('q');
    }
 
 //    public function findOneBySomeField($value): ?Question
