c3cf45705abc1ab05e87a8e5adb5686128f749c7
ljweston <lweston@ncees.org>
2022-06-23 14:17:11 -0400

    Implemented foundry for the question entity and implemented fixtures

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use App\Entity\Question;
 use App\Repository\QuestionRepository;
 use App\Service\MarkdownHelper;
-use DateTime;
 use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Question;
+use App\Factory\QuestionFactory;
 use DateTimeImmutable;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -11,29 +12,6 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // When we need to save we use the EntityManagerInterface itself and not the repo we made
-        $question = new Question();
-
-        $question->setName("Magic Missing Pants")
-            ->setSlug("magic-missing-pants-".rand(0, 1000))
-            ->setQuestion(<<<EOF
-            Hi! So... I'm having a *weird* day. Yesterday, I cast a spell
-            to make my dishes wash themselves. But while I was casting it,
-            I slipped a little and I think `I also hit my pants with the spell`.
-            When I woke up this morning, I caught a quick glimpse of my pants
-            opening the front door and walking out! I've been out all afternoon
-            (with no pants mind you) searching for them.
-            Does anyone have a spell to call your pants back?
-            EOF);
-            
-        if (rand(1, 10) > 2) {
-            $question->setAskedAt(new DateTimeImmutable(sprintf('-%d days', rand(1, 100))));
-        }
-
-        $question->setVotes(rand(-20, 50));
-
-        $manager->persist($question);
-
-        $manager->flush();
+        QuestionFactory::new()->createMany(20);
     }
 }
