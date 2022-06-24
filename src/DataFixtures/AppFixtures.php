0f894f394995a6b704ae248aecf9e195f8652291
ljweston <lweston@ncees.org>
2022-06-24 13:39:32 -0400

    Migrated changes for our Answer and Question to relate

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Answer;
 use App\Entity\Question;
 use App\Factory\QuestionFactory;
 use DateTime;
@@ -18,5 +19,20 @@ class AppFixtures extends Fixture
             ->unpublished()
             ->createMany(5)
         ;
+
+        $answer = new Answer();
+        $answer->setContent('This is a question test. I do not know the answer, lol.');
+        $answer->setUsername('weaverryan');
+
+        $question = new Question();
+        $question->setName('How to magically not be broke.');
+        $question->setQuestion('... I am broke. How do I make money?');
+        // set the relationship btwn ans and ques
+        $answer->setQuestion($question);
+
+        $manager->persist($answer);
+        $manager->persist($question);
+
+        $manager->flush();
     }
 }
