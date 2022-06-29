faff1aff878a499a89079bfa7f059c90eb503147
ljweston <lweston@ncees.org>
2022-06-29 16:53:54 -0400

    implementing new tag entity and question relation

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use App\Entity\Answer;
 use App\Entity\Question;
+use App\Entity\Tag;
 use App\Factory\AnswerFactory;
 use App\Factory\QuestionFactory;
 use DateTime;
@@ -32,6 +33,20 @@ class AppFixtures extends Fixture
                 'question' => $questions[array_rand($questions)],
             ];
         })->needsApproval()->many(20)->create();
+
+        $question = QuestionFactory::createOne();
+
+        $tag1 = new Tag();
+        $tag1->setName('Dinosaurs');
+        $tag2 = new Tag();
+        $tag2->setName('Monster Trucks');
+
+        // realte question to a tag!
+        $question->addTag($tag1);
+        $question->addTag($tag2);
+
+        $manager->persist($tag1);
+        $manager->persist($tag2);
 
         $manager->flush();
     }
