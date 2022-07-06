3894f97f2377123da0a2a7a6ebf7eaba2f05a123
ljweston <lweston@ncees.org>
2022-07-06 13:31:06 -0400

    Made a note about making or setting roles

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -71,6 +71,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         $roles = $this->roles;
         // guarantee every user at least has ROLE_USER
+        // ALL ROLES MUST START WITH "ROLE_"
         $roles[] = 'ROLE_USER';
 
         return array_unique($roles);
