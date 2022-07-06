0931f3ccbcd75c6513a01ec59e749539b3fdf1e7
ljweston <lweston@ncees.org>
2022-07-06 14:44:25 -0400

    Implemented AbstractLoginFormAuth base class
    Added required function getLoginUrl

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -13,7 +13,7 @@ use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Exception\UserNotFoundException;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Csrf\CsrfToken;
-use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
@@ -22,7 +22,7 @@ use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordC
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface;
 
-class LoginFormAuthenticator extends AbstractAuthenticator implements AuthenticationEntryPointInterface
+class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
 {
     private UserRepository $userRepository;
     private RouterInterface $router;
@@ -31,12 +31,6 @@ class LoginFormAuthenticator extends AbstractAuthenticator implements Authentica
     {
         $this->userRepository = $userRepository;
         $this->router = $router;
-    }
-    // beginning of each req symfony calls this method "Is there auth information on this req that you understand???
-    public function supports(Request $request): ?bool
-    {
-        // grab current url and check the method type return true if everything works out
-        return $request->getPathInfo() === '/login' && $request->isMethod('POST');
     }
 
     public function authenticate(Request $request): Passport
@@ -75,19 +69,8 @@ class LoginFormAuthenticator extends AbstractAuthenticator implements Authentica
         );
     }
 
-    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
-    {
-        // on failure redirect to our login page and STACHE the exception to the session
-        $request->getSession()->set(Security::AUTHENTICATION_ERROR, $exception);
-        return new RedirectResponse(
-            $this->router->generate('app_login')
-        );
-    }
-
-   public function start(Request $request, AuthenticationException $authException = null): Response
+   protected function getLoginUrl(Request $request): string
    {
-        return new RedirectResponse(
-            $this->router->generate('app_login')
-        );
+        return $this->router->generate('app_login');
    }
 }
