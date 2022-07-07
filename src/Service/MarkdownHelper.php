e9ff7fc09f5d31ec6594b5d145dfcc4b4fe55c5f
ljweston <lweston@ncees.org>
2022-07-07 16:38:00 -0400

    Added additional logic for the drop down menu

--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -4,6 +4,7 @@ namespace App\Service;
 
 use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
 use Psr\Log\LoggerInterface;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Contracts\Cache\CacheInterface;
 
 Class MarkdownHelper
@@ -12,19 +13,32 @@ Class MarkdownHelper
     private $cache;
     private $isDebug;
     private $logger;
+    private $security;
 
-    public function __construct(MarkdownParserInterface $markdownParser, CacheInterface $cache, bool $isDebug, LoggerInterface $markdownLogger)
+    public function __construct(MarkdownParserInterface $markdownParser, CacheInterface $cache, bool $isDebug, LoggerInterface $markdownLogger, Security $security)
     {
         $this->markdownParser = $markdownParser;
         $this->cache = $cache;    
         $this->debug = $isDebug; // in the services.yaml file we hardcoded this value as a param
         $this->logger = $markdownLogger; // named markdown logger to select it as our logger channel "markdown"
         // something like $mdLogger would use the main logger channel "app"
+        $this->security = $security;
     }
+    // pass markdown to be converted to HTML
     public function parse(string $source) : string
     {
         if (stripos($source, 'cat') !== false) {
             $this->logger->info('Meow');
+        }
+        // check to see if the user is logged in/ exists
+        if ($this->security->getUser()) {
+            $this->logger->info('Rendering markdown for {user}', [
+                'user' => $this->security->getUser()->getUserIdentifier(),
+            ]);
+        }
+
+        if ($this->isDebug) {
+            return $this->markdownParser->transformMarkdown($source);
         }
 
         return $this->cache->get('markdown'.md5($source), function() use ($source) {
