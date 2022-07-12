a86b2ec88efe73c91cfe34fd92167ea8fc3f1af6
ljweston <lweston@ncees.org>
2022-07-12 14:17:43 -0400

    Completed registration logic and verification email

--- src/EventSubscriber/CheckVerifiedUserSubscriber.php
+++ src/EventSubscriber/CheckVerifiedUserSubscriber.php
@@ -3,14 +3,25 @@
 namespace App\EventSubscriber;
 
 use App\Entity\User;
+use App\Security\AccountNotVerifiedAuthenticationException;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Exception\CustomUserMessageAccountStatusException;
 use Symfony\Component\Security\Http\Authenticator\Passport\UserPassportInterface;
 use Symfony\Component\Security\Http\Event\CheckPassportEvent;
+use Symfony\Component\Security\Http\Event\LoginFailureEvent;
 
 class CheckVerifiedUserSubscriber implements EventSubscriberInterface
 {
+    private RouterInterface $router;
+
+    public function __construct(RouterInterface $router)
+    {
+        $this->router = $router;
+    }
+
     public function onCheckPassport(CheckPassportEvent $event)
     {
         $passport = $event->getPassport();
@@ -24,18 +35,33 @@ class CheckVerifiedUserSubscriber implements EventSubscriberInterface
         }
 
         if (!$user->getIsVerified()) {
-            throw new CustomUserMessageAccountStatusException(
-                'Please verify your account before logging in'
-            );
+            throw new AccountNotVerifiedAuthenticationException();
+            
+            // throw new CustomUserMessageAccountStatusException(
+            //     'Please verify your account before logging in'
+            // );
             // throw new AuthenticationException(); // this class contains multiple types of exceptions
         }
+    }
+    // contains log event failure for incorrect credentials and no verification
+    public function onLoginFailure(LoginFailureEvent $event)
+    {
+        if (!$event->getException() instanceof AccountNotVerifiedAuthenticationException) {
+            return; // allow default failure behavior to do it's job
+        }
+        $response = new RedirectResponse(
+            $this->router->generate('app_verify_resend_email')
+        );
+
+        $event->setResponse($response);
     }
 
     public static function getSubscribedEvents()
     {
         return [
             // set the eventName and priority
-            CheckPassportEvent::class => ['onCheckPassport', -10]
+            CheckPassportEvent::class => ['onCheckPassport', -10],
+            LoginFailureEvent::class => 'onLoginFailure',
         ];
     }
 }
