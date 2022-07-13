2d8b3a4b018ee7fe781a1b0f068544dea5082124
ljweston <lweston@ncees.org>
2022-07-13 12:05:14 -0400

    Implemented logic for generating the users qr code

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,12 +2,16 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
+use Scheb\TwoFactorBundle\Security\TwoFactor\Provider\Totp\TotpAuthenticatorInterface;
+use Scheb\TwoFactorBundle\Security\TwoFactor\QrCode\QrCodeGenerator;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-class SecurityController extends AbstractController
+class SecurityController extends BaseController
 {
     #[Route('/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
@@ -21,5 +25,34 @@ class SecurityController extends AbstractController
     public function logout()
     {
         throw new \Exception('logout() should never be reached');
+    }
+
+    #[Route('/authenticate/2fa/enable', name: 'app_2fa_enable')]
+    #[IsGranted('ROLE_USER')] 
+    public function enable2fa(TotpAuthenticatorInterface $totpAuthenticator, EntityManagerInterface $entityManager)
+    {
+        $user = $this->getUser();
+        if(!$user->isTotpAuthenticationEnabled()) {
+            // create a secret
+            $user->setTotpSecret($totpAuthenticator->generateSecret());
+
+            $entityManager->flush();
+        }
+
+        return $this->render('security/enable2fa.html.twig');
+    }
+
+    /**
+     * @Route("/qr-code", name="qr_code")
+     */
+    #[Route('/authentication/2fa/qr-code', name: 'app_qr_code')]
+    #[IsGranted('ROLE_USER')]
+    public function displayGoogleAuthenticatorQrCode(QrCodeGenerator $qrCodeGenerator)
+    {
+        // $qrCode is provided by the endroid/qr-code library. See the docs how to customize the look of the QR code:
+        // https://github.com/endroid/qr-code
+        $qrCode = $qrCodeGenerator->getTotpQrCode($this->getUser());
+
+        return new Response($qrCode->writeString(), 200, ['Content-Type' => 'image/png']);
     }
 }
